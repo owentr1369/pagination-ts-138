f85305c502c672b2245475677b913d0b5043d9be
TranTam <tamvaa1306@gmail.com>
2022-06-21 17:05:40 +0700

    setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { check } from 'prettier';
+import { v4 as uuidV4 } from 'uuid';
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+console.log('Hello');
